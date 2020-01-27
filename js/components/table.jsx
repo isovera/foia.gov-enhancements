118fb868b9cb65dacdd76389d15cebf36ffd744a
Aaron Manire <amanire@isovera.com>
2020-01-27 15:28:11 -0500

    FOIA-302: Make Table component extend React component.

--- js/components/table.jsx
+++ js/components/table.jsx
@@ -3,9 +3,9 @@
  *
  */
 
-import React from 'react';
+import React, { Component } from 'react';
 
-class Table {
+class Table extends Component {
   render() {
     return (
       <table className="responsive-data-table">
