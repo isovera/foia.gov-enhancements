58da390df52cc7646247a0c91ce86dff7d411c63
Aaron Manire <amanire@isovera.com>
2020-01-27 14:24:29 -0500

    FOIA-302: Draft Table component.

---
+++ js/components/table.jsx
@@ -0,0 +1,31 @@
+/*
+ * HTML table
+ *
+ */
+
+import React from 'react';
+
+class Table {
+  render() {
+    return (
+      <table className="responsive-data-table">
+        <thead>
+          <tr>
+            <th>One</th>
+            <th>Two</th>
+            <th>Three</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td />
+            <td />
+            <td />
+          </tr>
+        </tbody>
+      </table>
+    );
+  }
+}
+
+export default Table;
