81dc2e50bf7b7cb9a5665847392ffaf3bcd0bdb0
Aaron Manire <amanire@isovera.com>
2020-01-28 11:30:24 -0500

    FOIA-302: Rename FoiaReportResultsTable component.

---
+++ js/components/foia_report_results_table.jsx
@@ -0,0 +1,32 @@
+/*
+ * HTML table
+ *
+ */
+
+import React, { Component, createRef } from 'react';
+// import Tabulator from "tabulator-tables";
+
+class FoiaReportResultsTable extends Component {
+  // constructor() {
+  //   super();
+
+  //   this.tabulatorElement = null;
+
+  //    tabulator = null;
+  //   tableData = [];
+  // }
+
+  // componentDidMount() {
+  //   this.tabulator = new Tabulator(this.el, {
+  //     data: this.tableData,
+  //     reactiveData:true,
+  //     columns: [],
+  //   });
+  // }
+
+  render() {
+    return (<div className="FoiaReportResultsTable" />);
+  }
+}
+
+export default FoiaReportResultsTable;
