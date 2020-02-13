8cb4e5f85bbe117596952b4b2648ef9ced5894ff
Jesse Day <jday@isovera.com>
2020-02-13 13:23:05 -0700

    FOIA-354: Make filtered api request in submit.
    * Updates the fetchAnnualReportData() function to take only a callback
    for simplicity of building all filters in the callback.
    * Builds filters for organizations and data types for the submit handler
    request.

--- js/components/foia_report_submit.jsx
+++ js/components/foia_report_submit.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { List } from 'immutable';
 import { reportActions } from '../actions/report';
+import annualReportDataFormStore from '../stores/annual_report_data_form';
 
 class FoiaReportDataSubmit extends Component {
   constructor(props) {
@@ -24,7 +26,26 @@ class FoiaReportDataSubmit extends Component {
     event.preventDefault();
     reportActions.returnFieldValidationStateOnSubmit();
     if (this.formIsValid()) {
-      reportActions.fetchAnnualReportData(this.props.selectedDataTypes);
+      reportActions.fetchAnnualReportData((builder) => {
+        const selectedAgencies = annualReportDataFormStore.getSelectedAgencies();
+        const agencies = selectedAgencies.filter(selection => selection.type === 'agency');
+        const components = selectedAgencies.filter(selection => selection.type === 'agency_component');
+        const includeOverall = agencies.filter((agency) => {
+          const overall = agency
+            .components
+            .filter(component => component.selected && component.isOverall);
+
+          return List.isList(overall) ? overall.size > 0 : overall.length > 0;
+        }).length > 0;
+
+
+        return builder
+          .includeSections(this.props.selectedDataTypes, includeOverall)
+          .addOrganizationsGroup({
+            agencies: agencies.map(agency => agency.abbreviation),
+            components: components.map(component => component.abbreviation),
+          });
+      });
     }
   }
 
